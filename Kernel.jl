c686151eae4bdcc1d0183abe922380f5fdfc4681
Yuichiro Tada <yuichiro.t.tada@gmail.com>
2024-07-08 09:56:27 +0900

    Is 50n+1 111--141 261--391

--- Kernel.jl
+++ Kernel.jl
@@ -348,7 +348,7 @@ function I2List(k)
     return I2List
 end;
 
-for i=261:10:291
+for i=111:10:141
     println("ki = ", i, "...")
     @time begin
         open(string("data/Is/I1_", i, ".dat"),"w") do out
